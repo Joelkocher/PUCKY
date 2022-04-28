bc70ee056429e7fe6e249aeefd56221fc227567b
Joël <joel.kocher@epfl.ch>
2022-04-28 08:41:52 +0200

    Removed old files with old name

--- CamReg_correction/pi_regulator.h
+++
@@ -1,7 +0,0 @@
-#ifndef PI_REGULATOR_H
-#define PI_REGULATOR_H
-
-//start the PI regulator thread
-void pi_regulator_start(void);
-
-#endif /* PI_REGULATOR_H */
--- CamReg_correction/process_image.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROCESS_IMAGE_H
-#define PROCESS_IMAGE_H
-
-float get_distance_cm(void);
-uint16_t get_line_position(void);
-void process_image_start(void);
-
-#endif /* PROCESS_IMAGE_H */
