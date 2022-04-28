c8c1c2925ce05afc8583501d941d9c17ca823ebb
rhesaber <rhea.saber@epfl.ch>
2022-04-28 09:14:00 +0200

    Modified process_image

--- Projet_robotique_BA6/process_image.c
+++ Projet_robotique_BA6/process_image.c
@@ -8,9 +8,55 @@
 
 #include <process_image.h>
 
-
 static float distance_cm = 0;
+static float distance_L1 = 0;
+static float distance_L8 = 0;
+static int angle_de_tournage=0;
 static uint16_t line_position = IMAGE_BUFFER_SIZE/2;	//middle
+
+
+uint16_t get_angle(float distance_L1, float distance_L8){
+
+	if(distance_L1> distance_L8)
+
+		if(distance_L1 > BORNE_MIN_L1_15 && distance_L1 < BORNE_MAX_L8_15 && distance_L8 > BORNE_MIN_L8_15 && distance_L8 < BORNE_MAX_L8_15){
+			      angle_de_tournage= DEG_15;
+		 }
+		if(distance_L1 > BORNE_MIN_L1_30 && distance_L1 < BORNE_MAX_L8_30 && distance_L8 > BORNE_MIN_L8_30 && distance_L8 < BORNE_MAX_L8_30){
+					angle_de_tournage= DEG_30;
+		 }
+		if(distance_L1 > BORNE_MIN_L1_45 && distance_L1 < BORNE_MAX_L8_45 && distance_L8 > BORNE_MIN_L8_45 && distance_L8 < BORNE_MAX_L8_45){
+					angle_de_tournage= DEG_45;
+		 }
+		if(distance_L1 > BORNE_MIN_L1_60 && distance_L1 < BORNE_MAX_L8_60 && distance_L8 > BORNE_MIN_L8_60 && distance_L8 < BORNE_MAX_L8_60){
+					angle_de_tournage= DEG_60;
+		 }
+		if(distance_L1 > BORNE_MIN_L1_75 && distance_L1 < BORNE_MAX_L8_75 && distance_L8 > BORNE_MIN_L8_75 && distance_L8 < BORNE_MAX_L8_75){
+					angle_de_tournage= DEG_75;
+		}
+    if(distance_L1< distance_L8)
+
+    	if(distance_L1 > BORNE_MIN_L1_15 && distance_L1 < BORNE_MAX_L8_15 && distance_L8 > BORNE_MIN_L8_15 && distance_L8 < BORNE_MAX_L8_15){
+					angle_de_tournage= -DEG_15;
+    	}
+    	if(distance_L1 > BORNE_MIN_L1_30 && distance_L1 < BORNE_MAX_L8_30 && distance_L8 > BORNE_MIN_L8_30 && distance_L8 < BORNE_MAX_L8_30){
+				     angle_de_tournage= -DEG_30;
+    	}
+		if(distance_L1 > BORNE_MIN_L1_45 && distance_L1 < BORNE_MAX_L8_45 && distance_L8 > BORNE_MIN_L8_45 && distance_L8 < BORNE_MAX_L8_45){
+					angle_de_tournage= -DEG_45;
+		 }
+		if(distance_L1 > BORNE_MIN_L1_60 && distance_L1 < BORNE_MAX_L8_60 && distance_L8 > BORNE_MIN_L8_60 && distance_L8 < BORNE_MAX_L8_60){
+					angle_de_tournage= -DEG_60;
+		}
+		if(distance_L1 > BORNE_MIN_L1_75 && distance_L1 < BORNE_MAX_L8_75 && distance_L8 > BORNE_MIN_L8_75 && distance_L8 < BORNE_MAX_L8_75){
+					angle_de_tournage= -DEG_75;
+		}
+    if(distance_L1 == distance_L8){
+	      	angle_de_tournage= DEG_90;
+	    }
+
+}
+
 
 //semaphore
 static BSEMAPHORE_DECL(image_ready_sem, TRUE);
@@ -19,85 +65,7 @@ static BSEMAPHORE_DECL(image_ready_sem, TRUE);
  *  Returns the line's width extracted from the image buffer given
  *  Returns 0 if line not found
  */
-uint16_t extract_line_width(uint8_t *buffer){
 
-	uint16_t i = 0, begin = 0, end = 0, width = 0;
-	uint8_t stop = 0, wrong_line = 0, line_not_found = 0;
-	uint32_t mean = 0;
-
-	static uint16_t last_width = PXTOCM/GOAL_DISTANCE;
-
-	//performs an average
-	for(uint16_t i = 0 ; i < IMAGE_BUFFER_SIZE ; i++){
-		mean += buffer[i];
-	}
-	mean /= IMAGE_BUFFER_SIZE;
-
-	do{
-		wrong_line = 0;
-		//search for a begin
-		while(stop == 0 && i < (IMAGE_BUFFER_SIZE - WIDTH_SLOPE))
-		{ 
-			//the slope must at least be WIDTH_SLOPE wide and is compared
-		    //to the mean of the image
-		    if(buffer[i] > mean && buffer[i+WIDTH_SLOPE] < mean)
-		    {
-		        begin = i;
-		        stop = 1;
-		    }
-		    i++;
-		}
-		//if a begin was found, search for an end
-		if (i < (IMAGE_BUFFER_SIZE - WIDTH_SLOPE) && begin)
-		{
-		    stop = 0;
-		    
-		    while(stop == 0 && i < IMAGE_BUFFER_SIZE)
-		    {
-		        if(buffer[i] > mean && buffer[i-WIDTH_SLOPE] < mean)
-		        {
-		            end = i;
-		            stop = 1;
-		        }
-		        i++;
-		    }
-		    //if an end was not found
-		    if (i > IMAGE_BUFFER_SIZE || !end)
-		    {
-		        line_not_found = 1;
-		    }
-		}
-		else//if no begin was found
-		{
-		    line_not_found = 1;
-		}
-
-		//if a line too small has been detected, continues the search
-		if(!line_not_found && (end-begin) < MIN_LINE_WIDTH){
-			i = end;
-			begin = 0;
-			end = 0;
-			stop = 0;
-			wrong_line = 1;
-		}
-	}while(wrong_line);
-
-	if(line_not_found){
-		begin = 0;
-		end = 0;
-		width = last_width;
-	}else{
-		last_width = width = (end - begin);
-		line_position = (begin + end)/2; //gives the line position.
-	}
-
-	//sets a maximum width or returns the measured width
-	if((PXTOCM/width) > MAX_DISTANCE){
-		return PXTOCM/MAX_DISTANCE;
-	}else{
-		return width;
-	}
-}
 
 static THD_WORKING_AREA(waCaptureImage, 256);
 static THD_FUNCTION(CaptureImage, arg) {
@@ -148,7 +116,7 @@ static THD_FUNCTION(ProcessImage, arg) {
 		}
 
 		//search for a line in the image and gets its width in pixels
-		lineWidth = extract_line_width(image);
+
 
 		//converts the width into a distance between the robot and the camera
 		if(lineWidth){
