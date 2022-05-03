f64154e45cf171a47a47ffce687d4756ada49eb8
Joël <joel.kocher@epfl.ch>
2022-05-03 13:54:27 +0200

    Image processing ready to be set up

--- Projet_robotique_BA6/process_image.c
+++ Projet_robotique_BA6/process_image.c
@@ -69,15 +69,7 @@ static THD_FUNCTION(ProcessImage, arg) {
 			image[i/2] = (uint8_t)img_buff_ptr[i]&0xF8;
 		}
 
-		//search for a line in the image and gets its width in pixels
-
-
-		//converts the width into a distance between the robot and the camera
-
 		/*
-		if(lineWidth){
-			distance_cm = PXTOCM/lineWidth;
-		}
 
 		if(send_to_computer){
 			//sends to the computer the image
--- Projet_robotique_BA6/process_image.h
+++ Projet_robotique_BA6/process_image.h
@@ -2,7 +2,6 @@
 #define PROCESS_IMAGE_H
 
 float get_distance_cm(void);
-uint16_t get_line_position(void);
 void process_image_start(void);
 
 #endif /* PROCESS_IMAGE_H */
