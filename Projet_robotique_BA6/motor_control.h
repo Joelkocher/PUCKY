217e0bebc76c06b6431bf8cf2c1ffbdd3f736c7d
rhesaber <rhea.saber@epfl.ch>
2022-05-03 10:27:34 +0200

    corrected merge errors

--- Projet_robotique_BA6/motor_control.h
+++ Projet_robotique_BA6/motor_control.h
@@ -1,12 +1,10 @@
 //#ifndef MOTOR_CONTROL_H
 //#define MOTOR_CONTROL_H
 
-<<<<<<< HEAD
+
 void turn_pucky(double angle);
 
 void motor_control_start();
-=======
-void turn_pucky(int turn_angle, int distance_L1, int distance_L8);
 
-#endif
->>>>>>> f0d0e86 (.)
+
+
--- Projet_robotique_BA6/proximity_sensors.c
+++ Projet_robotique_BA6/proximity_sensors.c
@@ -5,42 +5,29 @@
 #include <main.h>
 
 
-<<<<<<< HEAD
+
 #define SQUARE_COEFF		0.038
 #define LIN_COEFF			-3.827
 #define CONST_COEFF			93.788
 
-=======
+
 #define NB_OF_ANGLES 7
 #define NB_OF_VARIABLES 2
 #define OFFSET 34 //[mm]
->>>>>>> f0d0e86 (.)
 
-/*
-Angle is approximated with a quadratic function.
-It depends on ratio = distance_IR1/distance_IR8:
-
-angle(ratio)=0.038*ratio^2 - 3.827*ratio + 93.788
 
 static BSEMAPHORE_DECL(distance_reached, TRUE);
 static BSEMAPHORE_DECL(angle_captured, TRUE);
 
 
+/*
 
-void init_distance(void)
-{
-	VL53L0X_start();
-}
-uint16_t get_target_distance(void)
-{
-	return cos(get_angle())*(VL53L0X_get_dist_mm()-OFFSET);
-}
 
-void get_proximity()
-{
-	distance_IR1 = get_calibrated_prox(IR_FRONT_RIGHT);
-	distance_IR8 = get_calibrated_prox(IR_FRONT_LEFT);
-}
+
+
+
+
+
 */
 
 /*
@@ -60,7 +47,7 @@ double get_angle(double turn_angle, int distance_IR1, int distance_IR8){
 	double ratio;
 	double inv_ratio;
 
-<<<<<<< HEAD
+
 	ratio = distance_IR1/ distance_IR8;
 	inv_ratio = distance_IR8/ distance_IR1;
 
@@ -73,7 +60,7 @@ double get_angle(double turn_angle, int distance_IR1, int distance_IR8){
 		turn_angle = -(SQUARE_COEFF*inv_ratio*inv_ratio+LIN_COEFF*inv_ratio+CONST_COEFF);
 	}
 	return turn_angle;
-=======
+
 }
 
 static THD_WORKING_AREA(waTOF,0);
@@ -86,7 +73,7 @@ static THD_FUNCTION(TOF,arg) {
 
 	while(1){
 
-    	 get_target_distance;
+    	// get_target_distance;
     	 chBSemSignal(&distance_reached);
      }
 }
@@ -101,8 +88,8 @@ static THD_FUNCTION(Proximity,arg) {
      while(1){
 
     	 chBSemWait(&distance_reached);
-    	 get_proximity();
-    	 get_angle(distance_L1,distance_L8);
+    	// get_proximity();
+    	// get_angle();
 
     	 //thread 3 capture color et turn pucky dedans (moteur) turn_pucky();
     	 chBSemSignal(&angle_captured);
@@ -129,5 +116,5 @@ void proximity_and_tof_start(void){
 	chThdCreateStatic(waProximity, sizeof(waProximity), NORMALPRIO, Proximity, NULL);
 	chThdCreateStatic(waTOF, sizeof(waTOF), NORMALPRIO, TOF, NULL);
 	chThdCreateStatic(waCouleur, sizeof(waCouleur), NORMALPRIO, TOF, NULL);
->>>>>>> f0d0e86 (.)
+
 }
